e1257cd0e09fb06beacb15708710669e88a3c034
deangelica23 <deangelica.sanchez23@gmail.com>
2024-08-04 19:05:54 -0500

    finally hopefully

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,7 +16,10 @@ candidates_names = []
 candidate_votes = {}
 percentage_votes_won = []
 list_of_names = []
+candidates = []
 
+print("Election Results")
+print("----------------------------------")
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -27,6 +30,7 @@ with open(csvpath) as csvfile:
 # The total number of votes cast
     for row in csvreader:
         total_votes += 1
+       
         
         candidates_names = row[2]
         
@@ -37,38 +41,34 @@ with open(csvpath) as csvfile:
             candidate_votes[candidates_names] += 1
         else:
             candidate_votes[candidates_names] = 1  
-        
+
+# print(candidate_votes)       
 print(f"Total Votes: {total_votes}")
-
-
-print(f"Candidates Names: {candidates_names}" )
-for name in list_of_names:
-    print(name)
-
-#         #candidate = row[2]
-#         if candidate in candidate_votes:
-#             candidate_votes[candidate] += 1
-#             else:
-#                 candidate_votes[candidate] = 1
-
-# print(candidate_votes)
-
-
-
-
+print("----------------------------------")
 # A complete list of candidates who received votes
 
+#print(f"Candidates Names: {candidates_names}" )
+# for name in list_of_names:
+#     print(name)
 
 
 # The percentage of votes each candidate won
 
+
 # percentage_votes_won = {}
-#     for candidate, votes in candidate_votes.items():
-#         percentage = (votes / total_votes) * 100
+
+results = []
+for candidate, votes in candidate_votes.items():
+     percentage = (votes / total_votes) * 100
+     result_str = f"{candidate}: {percentage:.3f}% ({votes})"
+     results.append(result_str)
+     print(result_str)
+print("----------------------------------")
+winner = max(candidate_votes, key=candidate_votes.get)
+print(f"Winner: {winner}")
+print("----------------------------------")
 #         percentage_votes_won[candidate] = round(percentage, 3)
 # print(percentage_votes_won)
-
-
 
 
 
@@ -81,16 +81,21 @@ for name in list_of_names:
 
 # # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
-# output_file = "election_results.txt"
-# with open(output_file, 'w') as file:
-#     file.write("Election Results\n")
-#     file.write("-------------------------\n")
-#     file.write(f"Total Votes: {total_votes}\n")
-#     file.write("-------------------------\n")
-#     for candidate, votes in candidate_votes.items():
-#             file.write(f"{candidate}: {percentage_votes_won[candidate]}% ({votes})\n")
-#     file.write("-------------------------\n")
-#     file.write(f"Winner: {winner}\n")
-#     file.write("-------------------------\n")
 
-# print("Analysis results have been exported to 'election_results.txt'.")
+output_path = os.path.join('analysis', 'election_results.txt')
+with open(output_path, 'w') as file: 
+     file.write("Election Results\n")
+     file.write("-------------------------\n")
+     file.write(f"Total Votes: {total_votes}\n")
+     file.write("-------------------------\n")
+
+     #
+     for result in results: 
+          file.write(result + '\n')
+          #file.write(results + '\n')
+     file.write("-------------------------\n")
+
+     file.write(f"Winner: {winner}\n")
+     file.write("-------------------------\n")
+
+# # print("Analysis results have been exported to 'election_results.txt'.")
