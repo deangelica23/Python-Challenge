64f426ed7a92d1c75dcfc9713e76982a339e06d3
deangelica23 <deangelica.sanchez23@gmail.com>
2024-08-05 21:01:57 -0500

    hopefully the acutal last time

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,17 @@
 # PyPoll
 
-#import modules
+#import
 
 import os
 
 import csv
 
+#create the path
+
 csvpath = os.path.join("Resources", "election_data.csv")
 
 
-# In this Challenge, you are tasked with helping a small, rural town modernize its vote-counting process.
-# The dataset is composed of three columns: "Voter ID", "County", and "Candidate". 
-
+#variables
 
 total_votes = 0
 candidates_names = []
@@ -19,61 +19,53 @@ candidate_votes = {}
 percentage_votes_won = []
 list_of_names = []
 candidates = []
+results = []
 
-print("Election Results")
-print("----------------------------------")
+#opening path and reading
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
+
     header = next(csvreader)
 
 
 # The total number of votes cast
     for row in csvreader:
         total_votes += 1
-       
-        
         candidates_names = row[2]
-        
-        if candidates_names not in list_of_names:  #need this
-                list_of_names.append(candidates_names)  #need this
-        
+
+        if candidates_names not in list_of_names:
+                list_of_names.append(candidates_names)
+
         if candidates_names in candidate_votes:
             candidate_votes[candidates_names] += 1
         else:
-            candidate_votes[candidates_names] = 1  
+            candidate_votes[candidates_names] = 1
 
-# print(candidate_votes)       
+# start of print analysis
+print("Election Results")
+print("----------------------------------")
 print(f"Total Votes: {total_votes}")
 print("----------------------------------")
-# A complete list of candidates who received votes
 
+# list of candidates who received votes /print analysis
+# percentage of votes each candidate won /print analysis 
+# total number of votes each candidate won /print anaysis 
 
-# The percentage of votes each candidate won
-
-
-# percentage_votes_won = {}
-
-results = []
 for candidate, votes in candidate_votes.items():
      percentage = (votes / total_votes) * 100
      result_str = f"{candidate}: {percentage:.3f}% ({votes})"
      results.append(result_str)
      print(result_str)
+
+# The winner of the election based on popular vote
 print("----------------------------------")
 winner = max(candidate_votes, key=candidate_votes.get)
 print(f"Winner: {winner}")
 print("----------------------------------")
 
 
-# The total number of votes each candidate won
-
-# The winner of the election based on popular vote
-
-
-# # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-
+# export all the analysis results to a txt file
 
 output_path = os.path.join('analysis', 'election_results.txt')
 with open(output_path, 'w') as file: 
@@ -83,10 +75,7 @@ with open(output_path, 'w') as file:
      file.write("-------------------------\n")
      for result in results: 
           file.write(result + '\n')
-          #file.write(results + '\n')
      file.write("-------------------------\n")
-
      file.write(f"Winner: {winner}\n")
      file.write("-------------------------\n")
 
-# # print("Analysis results have been exported to 'election_results.txt'.")
